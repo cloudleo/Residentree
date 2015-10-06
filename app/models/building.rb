99323068c97fdfb53b50e1973bf602ba93ae6448
RaptorDog <randycastro20@gmail.com>
2015-10-06 18:06:31 -0400

    Vers. 1.0.4

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,16 @@
-class Building < ActiveRecord::Base
-	validates :address, :presence => {:message => "Please enter an address."}
+require "open-uri"
+require "json"
+require 'pry'
 
+class Building < ActiveRecord::Base
+  validates :address, :presence => {:message => "Please enter an address."}
+  
+  def get_result(address, borough)
+    "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=incident_address%20=%27#{address.upcase}%27&%$where=borough%20=%27#{borough}%27&$order=created_date%20DESC&$limit=1" 
+  end
+
+  def get_json(url)
+    return JSON.load(open(url))
+  end
 end
+
