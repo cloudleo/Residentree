c1ac8b9aaf35629de7e0e697fa6721da5d0068b9
cloudleo <l.castillo1989@hotmail.com>
2015-10-07 11:40:14 -0400

    added info for building on building show page

--- test.rb
+++
@@ -1,7 +0,0 @@
-require "pry"
-require 'soda/client'
-
-client = SODA::Client.new({:domain => 'data.cityofnewyork.us', :app_token => 'YWyMr1uyrgmiYaHafjeDzhk65'})
-binding.pry
-
-response = client.get("erm2-nwe9", {"$limit" => 10, :address => "860 Riverside Dr."})
