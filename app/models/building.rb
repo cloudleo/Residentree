98bac0aca61d51af94a89e55f180902cb693e945
cloudleo <l.castillo1989@hotmail.com>
2015-10-07 14:02:12 -0400

    added zipcode and link

--- app/models/building.rb
+++ app/models/building.rb
@@ -6,7 +6,7 @@ class Building < ActiveRecord::Base
   validates :address, :presence => {:message => "Please enter an address."}
   
   def get_result(address, borough)
-    "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=incident_address%20=%27#{address.upcase}%27&%$where=borough%20=%27#{borough}%27&$order=created_date%20DESC&$limit=20" 
+    "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=incident_address%20=%27#{address.upcase}%27&%$where=Borough%20=%27#{borough}%27&$order=created_date%20DESC&$limit=20" 
   end
 
   def get_json(url)
@@ -14,7 +14,7 @@ class Building < ActiveRecord::Base
   end
 
   def get_access(address)
-     return self.get_json(self.get_result(address, 'Manhattan'))
+     return self.get_json(self.get_result(address, 'MANHATTAN'))
      end
 end
 
