bd01a810984d67e1b5858ceef3836173443f0219
cloudleo <l.castillo1989@hotmail.com>
2015-10-07 11:36:26 -0400

    Added simple table for address page

--- app/models/building.rb
+++ app/models/building.rb
@@ -6,11 +6,15 @@ class Building < ActiveRecord::Base
   validates :address, :presence => {:message => "Please enter an address."}
   
   def get_result(address, borough)
-    "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=incident_address%20=%27#{address.upcase}%27&%$where=borough%20=%27#{borough}%27&$order=created_date%20DESC&$limit=1" 
+    "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$where=incident_address%20=%27#{address.upcase}%27&%$where=borough%20=%27#{borough}%27&$order=created_date%20DESC&$limit=20" 
   end
 
   def get_json(url)
     return JSON.load(open(url))
   end
+
+  def get_access(address)
+     return self.get_json(self.get_result(address, 'Manhattan'))
+     end
 end
 
