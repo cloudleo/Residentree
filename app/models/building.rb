ef98914861373a0aa036f3e062d82feca8456233
cloudleo <l.castillo1989@hotmail.com>
2015-10-11 18:25:24 -0400

    refactoring

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,5 +1,6 @@
 require "open-uri"
 require "json"
+
 require 'pry'
 
 class Building < ActiveRecord::Base
@@ -9,17 +10,23 @@ class Building < ActiveRecord::Base
   validates :address, :presence => {:message => "Please enter an address."}
   validates :borough, :presence => {:message => "Please choose a borough."}
   
-  def get_result(address, borough)
-    "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$order=created_date%20DESC&$limit=20&$where=incident_address%20=%27#{address.upcase}%27&%$where=Borough%20=%27#{borough}%27" 
-  end
+  # binding.pry
+  
 
-  def get_json(url)
-    return JSON.load(open(url))
-  end
+  # def get_result(address, borough)
+  #   "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$order=created_date%20DESC&$limit=20&$where=incident_address%20=%27#{address.upcase}%27&%$where=Borough%20=%27#{borough}%27" 
+  # end
+
+  # def get_json(url)
+  #   return JSON.load(open(url))
+  # end
 
   def get_access(address, borough)
     # binding.pry
-     return self.get_json(self.get_result(address.upcase, borough))
+  client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => "YWyMr1uyrgmiYaHafjeDzhk65"})
+  response = client.get("fhrw-4uyv", {"$order" => "created_date", "$limit" => 30, :incident_address => address, :borough => borough})
+     # 
+     return response
      end
 end
 
